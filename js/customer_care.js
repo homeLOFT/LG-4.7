f1f4adb2e93ed7632780aa99bc6c4e9f3214ce89
JW <jeromewilkers@gmail.com>
2015-05-21 17:21:56 -0700

    Customer Care Modal
    commit f7badc590036c5dddbed277c6eac5ee4bb870797
    Author: JW <jeromewilkers@gmail.com>
    Date:   Thu May 21 17:17:07 2015 -0700
    
        Customer Care Modal
    
        commit fc6dfd3efe7a203400905256f6334100a473878e
        Author: JW <jeromewilkers@gmail.com>
        Date:   Thu May 21 17:14:50 2015 -0700
    
            Customer Care Modal
    
            Prevent navigation to another url when selecting the customer care modal
    
        commit a3d41225cfdc290b56a1739cbedc6e71c0d67bc2
        Author: JW <jeromewilkers@gmail.com>
        Date:   Thu May 21 14:21:17 2015 -0700
    
            Customer Care Modal
    
            Attempt to fix show/hide effect on customer care modal

--- js/customer_care.js
+++ js/customer_care.js
@@ -1,38 +1,39 @@
-$(document).ready(function() {
-	
-	$(".custcare-link").click(function (event) {
-    
-		$( "#cc-modal" ).dialog({
- 	      autoOpen: false,
-		  closeOnEscape: true,
-		  width: 'auto',
-    	  height: 'auto',
-    	  maxWidth: 2000,
-		  modal: true,
-    	  fluid: true, //new option
-    	  resizable: false,
-		  draggable:false,
-    	  show: {
-    	    effect:'slide',
-    	    duration:1500,
-    	  },
-    	  hide: {
-    	    effect:'slide',
-    	    duration:1500,
-    	  },
-		  dialogClass: 'ccmodal',
-		  buttons: {
-		  	"Close Window": function () {
-            	$("#cc-modal").dialog("close");
-         	 }
-		  }
-    	});
-		
-		$(".ui-dialog-titlebar").hide();
-	
-    	$.get( "/common/modal/pages/customer-care.html", function( html ) {
-			
-      		$( "#cc-modal" ).html( html ).dialog('open');
-    	});
-	});	
+$(document).ready(function () {
+
+    $(".custcare-link").click(function (event) {
+        event.preventDefault();
+        $("#cc-modal").dialog({
+            autoOpen: false,
+            closeOnEscape: true,
+            width: 'auto',
+            height: 'auto',
+            maxWidth: 2000,
+            modal: true,
+            fluid: true, //new option
+            resizable: false,
+            draggable: false,
+            dialogClass: 'ccmodal',
+            buttons: {
+                "Close Window": function () {
+                    $("#cc-modal").dialog("close");
+                }
+            }
+        });
+
+        $(".ui-dialog-titlebar").hide();
+
+        $.get("/common/modal/pages/customer-care.html", function (html) {
+
+            $("#cc-modal").html(html).dialog('open', {
+                show: {
+                    effect: 'slide',
+                    duration: 1500
+                },
+                hide: {
+                    effect: 'slide',
+                    duration: 1500
+                }
+            });
+        });
+    });
 });
